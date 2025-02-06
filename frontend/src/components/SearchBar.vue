1804ccc28169ede20fe8e7a5b4898f8acd353218
Fares kouki <fares.kouki@esprit.tn>
2025-02-06 10:58:33 +0100

    second commit

--- frontend/src/components/SearchBar.vue
+++ frontend/src/components/SearchBar.vue
@@ -1,36 +1,32 @@
+<script setup>
+import { ref, watch } from 'vue';
+import axios from 'axios';
+
+const query = ref('');
+const results = ref([]);
+
+const search = async () => {
+  if (query.value.length >= 1) {
+    try {
+      const response = await axios.get(`http://localhost:5000/search?q=${query.value}`);
+      results.value = response.data;
+    } catch (error) {
+      console.error('Error fetching data:', error);
+    }
+  } else {
+    results.value = [];
+  }
+};
+
+// Automatically call search when `query` changes
+watch(query, search);
+</script>
+
 <template>
-    <div>
-      <input v-model="query" @input="search" type="text" placeholder="Search..." />
-      <ul>
-        <li v-for="item in results" :key="item.id">{{ item.name }}</li>
-      </ul>
-    </div>
-  </template>
-  
-  <script>
-  import axios from 'axios';
-  
-  export default {
-    data() {
-      return {
-        query: '',
-        results: [],
-      };
-    },
-    methods: {
-      async search() {
-        if (this.query.length >= 1) {
-          try {
-            const response = await axios.get(`http://localhost:5000/search?q=${this.query}`);
-            this.results = response.data;
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
-        } else {
-          this.results = [];
-        }
-      },
-    },
-  };
-  </script>
-  
+  <div>
+    <input v-model="query" type="text" placeholder="Search..." />
+    <ul>
+      <li v-for="item in results" :key="item.id">{{ item.name }}</li>
+    </ul>
+  </div>
+</template>
